d72c5c5812e30701e346f6bb65bf266ae45635a6
Javier Yzaguirre <47618267+inglorious-ratbastard@users.noreply.github.com>
2026-03-19 13:56:41 -0500

    Update PostConstruct import from javax to jakarta

--- botnSoccer/src/main/java/com/backofthenet/soccer/DataCacheService.java
+++ botnSoccer/src/main/java/com/backofthenet/soccer/DataCacheService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
